0ca6f363157a4e23fe6cf1329c90003666ab471b
halso <halso@localhost>
2024-04-24 08:38:32 +0100

    Modified Java code to add new sentence

--- src/main/java/com/test/TestMain.java
+++ src/main/java/com/test/TestMain.java
@@ -3,7 +3,7 @@ package com.test;
 public class TestMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello. This is SteveTestMain.");
+		System.out.println("Hello. This is SteveTestMain. Which has now been modified!");
 	}
 
 }
